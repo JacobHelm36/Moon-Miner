40a648940dbd1b4bd39b3da32183ded7d39e025a
jhelm3 <jakehelm36@gmail.com>
2020-02-04 16:58:37 -0700

    Can't purchase items without enough resources

--- app/services/GameService.js
+++ app/services/GameService.js
@@ -10,13 +10,22 @@ let _Moon = new Moon({cheeseCount: 100, clickInit: 1})
 
 export default class GameService {
   pick() {
+    if (_Moon.cheeseCount >= _Pick.price) {
     _Moon.cheeseCount -= _Pick.price
     _Moon.clickInit += _Pick.clickDam
+    _Pick.price += Math.floor(_Pick.price*.5)
+    } else {
+      document.getElementById("").textContent = "The pick costs more than you have!!"
+    }
   }
 
   cart() {
+    if (_Moon.cheeseCount >= _Cart.price) {
     _Moon.cheeseCount -= _Cart.price
     _Moon.clickInit += _Cart.clickDam
+    } else {
+      document.getElementById("").textContent = "The cart costs more than you have!"
+    }
   }
 
   click() {
